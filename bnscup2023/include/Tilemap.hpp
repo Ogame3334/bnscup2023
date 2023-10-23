708a5332e9a57696f2262a46f1d64a36f6b80ba6
おがめ <kazuhiroisobe0522@gmail.com>
2023-10-23 11:34:14 +0900

    一旦commit

---
+++ bnscup2023/include/Tilemap.hpp
@@ -0,0 +1,32 @@
+﻿#include <Siv3D.hpp>
+
+namespace bnscup2023 {
+	enum class TileId {
+		AIR,
+		NORMAL_GRAND,
+	};
+	class Tile {
+	private:
+		inline static int TileSize = 10;
+		Texture texture;
+	public:
+		Tile() = default;
+		Tile(String texture_name) : texture(Texture{ texture_name }) {}
+		Tile(Texture t) : texture(t) {}
+		void setTexture(Texture t) { this->texture = t; }
+		void setTexture(String texture_name) { this->texture = Texture{ texture_name }; }
+		Texture getTexture() const noexcept { return this->texture; }
+		void draw() const;
+	};
+
+	namespace Tiles {
+		const Tile air{U"air"};
+	}
+
+	class TileMap {
+	private:
+	public:
+		void update();
+		void draw() const;
+	};
+}
