213af04bc5c097d2b2b2eacd82203a738b70ac4a
おがめ <kazuhiroisobe0522@gmail.com>
2023-10-25 01:23:43 +0900

    Tileへのメソッド追加

--- bnscup2023/include/Tiles/Tiles.hpp
+++ bnscup2023/include/Tiles/Tiles.hpp
@@ -3,9 +3,22 @@
 #include "BaseTile.hpp"
 
 namespace bnscup2023 {
+	class ErrorTile : public BaseTile {
+	public:
+		ErrorTile() = default;
+		ErrorTile(String texture_name) { this->texture_asset_name = texture_name; }
+	};
 	class AirTile : public BaseTile {
 	public:
 		AirTile() = default;
 		AirTile(String texture_name) { this->texture_asset_name = texture_name; }
+	};
+	class NormalGroundTile : public BaseTile {
+	public:
+		NormalGroundTile() = default;
+		NormalGroundTile(String texture_name) {
+			this->texture_asset_name = texture_name;
+			this->isCollisionable = true;
+		}
 	};
 }
