369258e4aaed1dc889b47c4107e8b92cbcc88c87
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-10-25 01:53:49 +0900

    一旦commit

---
+++ bnscup2023/include/Player.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+namespace bnscup2023 {
+	enum class PLAYERPOSE {
+		LEFT,
+		LEFT_WALK,
+		LEFT_JUMP,
+		RIGHT,
+		RIGHT_WALK,
+		RIGHT_JUMP,
+	};
+
+	class Player {
+	private:
+		Vec2 pos;
+		Vec2 vel;
+		Point focusedPos;
+		// TODO: プレイヤーアセットの保持
+		PLAYERPOSE pose;
+		
+	public:
+		Player() {}
+
+		bool access();
+		void update();
+		void draw() const;
+	};
+}
---
+++ bnscup2023/src/Player.cpp
@@ -0,0 +1,14 @@
+﻿#include "../include/Player.hpp"
+
+namespace bnscup2023 {
+	bool Player::access() {
+		// TODO: 注目しているタイルのアクセス呼び出し
+		return false;
+	}
+	void Player::update() {
+		// TODO: キー入力に応じたプレイヤーの移動，マップ内のタイルとの衝突判定
+	}
+	void Player::draw() const {
+		// TODO: 保持したプレイヤーアセットの描画
+	}
+}
