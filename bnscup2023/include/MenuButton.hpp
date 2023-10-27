e878da9950e7f3be6751adf7a956a7e19aaf316e
おがめ <kazuhiroisobe0522@gmail.com>
2023-10-28 00:37:49 +0900

    多分ボタンの実装完了

---
+++ bnscup2023/include/MenuButton.hpp
@@ -0,0 +1,92 @@
+﻿#pragma once
+
+#include <Siv3D.hpp>
+
+namespace bnscup2023 {
+	class MenuButton {
+	private:
+		std::function<void(void)> handler = nullptr;
+		String display_text = U"Button";
+		Rect display_rect = Rect{100, 100, 200, 50};
+		Color normal_color = Palette::Lightgray;
+		Color active_color = Palette::Skyblue;
+		bool is_active = false;
+
+		std::shared_ptr<MenuButton> up = nullptr;
+		std::shared_ptr<MenuButton> right = nullptr;
+		std::shared_ptr<MenuButton> down = nullptr;
+		std::shared_ptr<MenuButton> left = nullptr;
+	public:
+		MenuButton() = default;
+		MenuButton(std::function<void(void)> h) : handler(h) {}
+
+		void setFunction(std::function<void(void)> h) { this->handler = h; }
+		void execute() { if (static_cast<bool>(this->handler)) this->handler(); }
+
+		inline void setActive(bool _isActive) { this->is_active = _isActive; }
+		MenuButton& setRectPos(int x, int y) {
+			this->display_rect.x = x;
+			this->display_rect.y = y;
+			return *this;
+		}
+		MenuButton& setRectSize(int w, int h) {
+			this->display_rect.w = w;
+			this->display_rect.h = h;
+			return *this;
+		}
+		MenuButton& setText(String text) {
+			this->display_text = text;
+			return *this;
+		}
+
+		void draw() const;
+	};
+
+	class MenuButtonManager {
+	private:
+		class MenuButtonContainer {
+		private:
+			struct quartet {
+				int up = -1;
+				int right = -1;
+				int down = -1;
+				int left = -1;
+			};
+			quartet connection;
+			MenuButton button;
+		public:
+			MenuButtonContainer() = default;
+			MenuButtonContainer(std::function<void(void)> h) : button(MenuButton{ h }) {}
+
+			MenuButtonContainer& setUp(int i) { this->connection.up = i; return *this; }
+			MenuButtonContainer& setRight(int i) { this->connection.right = i; return *this; }
+			MenuButtonContainer& setDown(int i) { this->connection.down = i; return *this; }
+			MenuButtonContainer& setLeft(int i) { this->connection.left = i; return *this; }
+			MenuButton getButton() const { return this->button; }
+			MenuButton& getButtonRef() { return this->button; }
+			quartet getConnection() const { return this->connection; }
+			quartet& getConnectionRef() { return this->connection; }
+		};
+		Array<MenuButtonContainer> menu_buttons;
+		int now_selected = -1;
+	public:
+		MenuButtonManager() = default;
+		MenuButtonManager(bool isDefaultSelect) : now_selected(isDefaultSelect ? 0 : -1) {}
+
+		MenuButtonContainer operator[] (int index) { return this->menu_buttons[index]; }
+
+		MenuButton& add(std::function<void(void)> h) {
+			this->menu_buttons.push_back(MenuButtonContainer{ h });
+			if (this->now_selected == this->menu_buttons.size() - 1) this->menu_buttons.back().getButtonRef().setActive(true);
+			return this->menu_buttons.back().getButtonRef();
+		}
+
+		void setConnection(int index, int up = -1, int right = -1, int down = -1, int left = -1) {
+			this->menu_buttons[index].getConnectionRef() = { up, right, down, left };
+		}
+		MenuButtonContainer& at(int index) { return this->menu_buttons[index]; }
+
+		void update();
+		void draw() const;
+	};
+}
---
+++ bnscup2023/include/Scenes/OgameTest.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "../SceneConfig.hpp"
+#include "../MenuButton.hpp"
+
+class OgameTest : public App::Scene
+{
+private:
+	TileMap tile_map = TileMap{ getData().tile_assets, U"TestField.csv" };
+	MenuButtonManager mbm{ true };
+public:
+	OgameTest(const InitData& init)
+		: IScene{ init }
+	{
+		mbm.add([] {Print << U"Hi!!"; }).setText(U"Hi!!");
+		mbm.add([] {Print << U"Hoge!!"; }).setText(U"Hoge!!").setRectPos(100, 200);
+		mbm.at(0).setUp(1).setDown(1);
+		mbm.at(1).setUp(0).setDown(0);
+	}
+
+	void update() override
+	{
+		mbm.update();
+	}
+
+	void draw() const override
+	{
+		//getData().tile_assets[1].drawAt(Scene::CenterF());
+		//getData().tile_assets[U"normal_ground"].drawAt(Scene::CenterF());
+		this->tile_map.draw();
+		mbm.draw();
+	}
+};
