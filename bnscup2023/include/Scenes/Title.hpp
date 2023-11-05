d9e5f61030fb7c4ade13b4937a79e004582afb75
おがめ <kazuhiroisobe0522@gmail.com>
2023-11-05 10:49:02 +0900

    titleに背景とbgmを追加

--- bnscup2023/include/Scenes/Title.hpp
+++ bnscup2023/include/Scenes/Title.hpp
@@ -7,6 +7,7 @@ class TitleScene : public SceneBase
 {
 private:
 	MenuManager mm{ true };
+
 public:
 	TitleScene(const InitData& init)
 		: SceneBase{ init }
@@ -15,6 +16,11 @@ public:
 		this->mm.setIsBack(false);
 		this->mm.setIsCanEscape(false);
 		this->MenuButtonInit(0);
+		AudioAsset(U"titlebgm").play();
+	}
+
+	~TitleScene() {
+		AudioAsset(U"titlebgm").stop();
 	}
 
 	void MenuButtonInit(int i);
@@ -31,9 +37,8 @@ public:
 
 	void draw() const override
 	{
-		Scene::SetBackground(ColorF{ 0.3, 0.4, 0.5 });
+		TextureAsset(U"background").draw(0, 0);
 		FontAsset(U"TitleFont")(U"Water Adventure").drawAt(640, 200);
-		//FontAsset(U"Default")(U"Press Space Key").drawAt(640, 360);
 		this->mm.draw();
 	}
 };
